cba60466a16be6515a061bf897107ea2a168e59a
Your Username <you@example.com>
2021-02-11 12:47:14 -0500

    romina-velarde

--- Phase-4-Round-1/src/components/AccountContainer.js
+++ Phase-4-Round-1/src/components/AccountContainer.js
@@ -4,12 +4,42 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+
+  state = {
+    transactions: [],
+    searchText: ''
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:6001/transactions')
+    .then(r => r.json())
+    .then(transData => this.setState({
+      transactions: transData
+    }))
+    // .then(console.log)
+  }
+
+  addTransaction = (newTrans) =>{
+    // console.log(newTrans)
+    this.setState({
+      transactions: [...this.state.transactions, newTrans]
+    })
+  }
+
+  searchBar = (textT)=>{
+    // console.log(textT)
+    this.setState({
+      searchText: textT
+    })
+  }
+
   render() {
+  const filteredTrans = this.state.transactions.filter(filterTrans => filterTrans.description.toLowerCase().includes(this.state.searchText))
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search searchBar={this.searchBar} searchText={this.state.searchText}/>
+        <AddTransactionForm addTransaction={this.addTransaction}/>
+        <TransactionsList transactions={filteredTrans}/>
       </div>
     );
   }
--- Phase-4-Round-1/src/components/AddTransactionForm.js
+++ Phase-4-Round-1/src/components/AddTransactionForm.js
@@ -1,15 +1,46 @@
 import React, { Component } from "react";
 
 class AddTransactionForm extends Component {
+
+  state = {
+    date: '',
+    description: '',
+    category: '',
+    amount: ''
+  }
+
+  handleSubmit = (e)=>{
+    e.preventDefault()
+    // console.log(this.state)
+    let newTrans = {
+      date: this.state.date,
+      description: this.state.description,
+      category: this.state.category,
+      amount:this.state.amount
+    }
+    // console.log(newTrans)
+    let reqPack={
+      headers: {"Content-Type":"application/json"},
+      method: 'POST',
+      body: JSON.stringify(newTrans)
+    }
+    // console.log(reqPack)
+    fetch('http://localhost:6001/transactions', reqPack)
+    .then(r => r.json())
+    .then(postedTrans => this.props.addTransaction(postedTrans))
+    // .then(console.log)
+
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.handleSubmit} className="ui form">
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
-            <input
+            <input onChange={(e)=> this.setState({date: e.target.value})} type="date" name="date" />
+            <input onChange={(e)=> this.setState({description: e.target.value})} type="text" name="description" placeholder="Description" />
+            <input onChange={(e)=> this.setState({category: e.target.value})} type="text" name="category" placeholder="Category" />
+            <input onChange={(e)=> this.setState({amount: e.target.value})}
               type="number"
               name="amount"
               placeholder="Amount"
