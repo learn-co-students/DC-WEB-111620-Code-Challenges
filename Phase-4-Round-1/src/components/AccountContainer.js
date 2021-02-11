15a75ae4338266bf49e16529305db843972af923
akrama23 <arundelfootball23@gmail.com>
2021-02-11 11:30:23 -0500

    first

--- Phase-4-Round-1/src/components/AccountContainer.js
+++ Phase-4-Round-1/src/components/AccountContainer.js
@@ -4,12 +4,22 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+
+  state = {
+    transactions: []
+  }
+
+  componentDidMount = () => {
+    fetch("http://localhost:6001/transactions")
+      .then(r => r.json())
+      .then(transactions => this.setState({transactions}))
+  }
   render() {
     return (
       <div>
         <Search />
         <AddTransactionForm />
-        <TransactionsList />
+        <TransactionsList transactions={this.state.transactions} />
       </div>
     );
   }
