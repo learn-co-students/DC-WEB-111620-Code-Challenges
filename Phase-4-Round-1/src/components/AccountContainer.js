fd1453c88b7c82cf890023fd060eaa3722831c26
Lewiss-codes <ldp231@yahoo.com>
2021-02-11 12:19:56 -0600

    luis-penaloza

--- Phase-4-Round-1/src/components/AccountContainer.js
+++ Phase-4-Round-1/src/components/AccountContainer.js
@@ -2,14 +2,34 @@ import React, { Component } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
+const URL = "http://localhost:6001/transactions/"
 
 class AccountContainer extends Component {
+  state = {
+    transactions: [],
+    search: ""
+  }
+  componentDidMount() {
+    fetch(URL)
+      .then(ren => ren.json())
+      .then(transactions => this.setState({transactions}))
+  }
+  addNewTransaction = (newTransaction) => {
+    this.setState({
+      transactions: [...this.state.transactions, newTransaction]
+    })
+  }
+  handleSearch = (e) => {
+    this.setState({
+      search: e.target.value
+    })
+  }
   render() {
     return (
       <div>
-        <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <Search search={this.state.search} handleSearch={this.handleSearch}/>
+        <AddTransactionForm addNewTransaction={this.addNewTransaction}/>
+        <TransactionsList transactions={this.state.transactions.filter(transaction => transaction.description.toLowerCase().includes(this.state.search))}/>
       </div>
     );
   }
--- Phase-4-Round-1/src/components/Search.js
+++ Phase-4-Round-1/src/components/Search.js
@@ -1,14 +1,13 @@
 import React from "react";
 
-const Search = () => {
+const Search = (search, handleSearch) => {
   return (
     <div className="ui large fluid icon input">
       <input
         type="text"
         placeholder={"Search your Recent Transactions"}
-        onChange={() => {
-          console.log("Searching...");
-        }}
+        value={search}
+        onChange={handleSearch}
       />
       <i className="circular search link icon"></i>
     </div>
